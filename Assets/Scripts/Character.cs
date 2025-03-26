9af047d30f3139b8f2d00efe1e3338356b10e846
Niniutt <72707364+Niniutt@users.noreply.github.com>
2025-03-26 12:05:03 +0100

    player movement

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed = 20.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+    }
+}
