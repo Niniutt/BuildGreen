fab35f9794f35e131b56fb7c8ae88492f0128795
Niniutt <marine.d.niut@gmail.com>
2025-05-22 15:37:30 +0200

    First-mini game done!
    not integrated tho

--- Assets/0Scripts/MiniGame.cs
+++ Assets/0Scripts/MiniGame.cs
@@ -1,16 +1,78 @@
+using TMPro;
 using UnityEngine;
 
 public class MiniGame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int gameDuration = 10;
+    private int timer = 0;
+    private float startDelay = 0f;
+    private float repetitionDelay = 1f;
+    private int score = 0;
+    private int errors = 0;
+    private readonly int maxErrors = 3;
+    public bool hasStarted = false;
+
+    // UI
+    [SerializeField] private TMP_Text timeUI;
+    [SerializeField] private TMP_Text scoreUI;
+    [SerializeField] private TMP_Text errorUI;
+
+    public void Start()
     {
-        
+        MiniGameStart();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MiniGameStart()
     {
-        
+        MiniGameInit();
+
+        InvokeRepeating(nameof(UpdateMiniGame), startDelay, repetitionDelay);
+    }
+
+    private void MiniGameInit(int gameDuration = 10, int score = 0, int errors = 0)
+    {
+        this.gameDuration = gameDuration;
+        this.score = score;
+        this.errors = errors;
+
+        timer = this.gameDuration;
+        hasStarted = true;
+
+        UpdateErrors(0);
+    }
+
+    private void UpdateMiniGame()
+    {
+        if (hasStarted)
+        {
+            UpdateTime();
+        }
+        if (timer == 0 || errors == maxErrors)
+        {
+            EndMiniGame();
+        }
+    }
+
+    public void EndMiniGame()
+    {
+        hasStarted = false;
+    }
+
+    public void UpdateTime()
+    {
+        timer -= 1;
+        timeUI.text = "Time left: " + timer;
+    }
+
+    public void UpdateScore(int newScore)
+    {
+        score += newScore;
+        scoreUI.text = "Score: " + score;
+    }
+
+    public void UpdateErrors(int newErrors)
+    {
+        errors += newErrors;
+        errorUI.text = "Errors: " + errors;
     }
 }
--- Assets/0Scripts/TextMinimizing.cs
+++ Assets/0Scripts/TextMinimizing.cs
@@ -1,16 +1,87 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TextMinimizing : MonoBehaviour
+public class TextMinimizing : MiniGame
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Logic
+    [SerializeField] private TMP_Text sentence;
+    private string solution;
+    private string current;
+    private bool comment;
+
+    private readonly string[] sentences = new string[]{ "print('Yo') % Hi", "array =    ['Yo', 'Lo']", "number = 42"};
+    private readonly string[] solutions = new string[] { "print('Yo')", "array=['Yo','Lo']", "number=42" };
+
+    // Animations
+    [SerializeField] private TMP_Text mark;
+    [SerializeField] private RawImage FrameX;
+    [SerializeField] private RawImage FrameS;
+    private readonly Color baseColor = Color.white;
+    private readonly Color correct = Color.green;
+    private readonly Color wrong = Color.red;
+
     void Start()
     {
-        
+        base.Start();
+
+        int index = Random.Range(0, sentences.Length - 1);
+        solution = solutions[index];
+        sentence.text = sentences[index];
+        current = sentences[index];
+        comment = false;
+        hasStarted = true;
+
+        InvokeRepeating(nameof(AnimateText), 0f, 0.5f); // Alright with hardcoded here
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AnimateText()
     {
-        
+        mark.enabled = !mark.enabled;
+    }
+
+    private void Update()
+    {
+        if (!hasStarted) return;
+        char c = current[0];
+        bool isUseful;
+        if (c == '%') comment = true;
+        if (c == ' ' || comment) isUseful = false;
+        else isUseful = true;
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (isUseful)
+            {
+                FrameX.color = correct;
+                RemoveCharacter();
+                UpdateScore(1);
+            }
+            else { FrameX.color = wrong; UpdateErrors(1); }
+            FrameS.color = baseColor;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isUseful)
+            {
+                FrameS.color = correct;
+                RemoveCharacter();
+                UpdateScore(1);
+            }
+            else { FrameS.color = wrong; UpdateErrors(1); }
+            FrameX.color = baseColor;
+        }
+        if (current == "")
+        {
+            EndMiniGame();
+            FrameX.color = baseColor;
+            FrameS.color = baseColor;
+        }
+    }
+
+    void RemoveCharacter()
+    {
+        current = current.Substring(1);
+        sentence.text = current;
     }
 }
